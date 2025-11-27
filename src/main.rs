e800eb5a6495f4e5752b258004963ca701ddd34e
IhsanMaul56 <sansgrids5601@gmail.com>
2025-11-27 13:20:40 +0700

    feat: Add test functions demonstrating basic Rust concepts like variables and mutability.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn hello() {
+    println!("Aku aslinya dua orang");
+}
+
+#[test]
+fn variable() {
+    let name = "Sansgrids";
+    let umur = 21;
+    let umur_asli = umur + 1;
+    println!("My name is {} and i'm {} years old", name, umur_asli);
+}
+
+#[test]
+fn mutable() {
+    let mut name = "Sansgrids";
+    println!("Hello, {}", name);
+
+    name = "Ihsan Gadun";
+    println!("Hello, {}", name);
+    
+    name = "Jekol";
+    println!("Hello, {}", name);
+}
